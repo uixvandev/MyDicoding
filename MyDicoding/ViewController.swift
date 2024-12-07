86eee51555e0ba02b107a0fc6e77c620f8dbb112
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2024-12-07 21:35:12 +0700

    Initial Commit

---
+++ MyDicoding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDicoding
+//
+//  Created by irfan wahendra on 07/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
